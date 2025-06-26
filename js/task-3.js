fdc74f519a1f8e2f9399900b0763a70cb0cf1838
Baran Taşcı <barantasci44@gmail.com>
2025-06-26 21:52:11 +0300

    Ödev#7

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const inputEl = document.getElementById("name-input");
+const outputEl = document.getElementById("name-output");
+
+inputEl.addEventListener("input", () => {
+  const trimmedValue = inputEl.value.trim();
+
+  outputEl.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor(); // Rastgele hex renk al
+  body.style.backgroundColor = newColor; // <body> arka plan rengini değiştir
+  colorSpan.textContent = newColor; // <span class="color"> içine aynı rengi yaz
+});
